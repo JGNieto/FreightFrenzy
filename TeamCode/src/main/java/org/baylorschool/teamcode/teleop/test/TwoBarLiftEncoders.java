144cf144d1ba51fed896cbe79776ee1bf0a25efe
Eric Zheng <greenserpent2007@gmail.com>
2021-11-10 17:07:44 -0500

    Two bar encoders

---
+++ TeamCode/src/main/java/org/baylorschool/teamcode/teleop/test/TwoBarLiftEncoders.java
@@ -0,0 +1,50 @@
+package org.baylorschool.teamcode.teleop.test;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+@TeleOp(name="TwoBarLiftEncoders", group="Test")
+public class TwoBarLiftEncoders extends LinearOpMode {
+    private DcMotor roller;
+    private DcMotor twobar1;
+
+    @Override
+    public void runOpMode() {
+        roller = hardwareMap.get(DcMotor.class, "roller");
+        twobar1 = hardwareMap.get(DcMotor.class, "twobar1");
+
+        roller.setDirection(DcMotor.Direction.FORWARD);
+        twobar1.setDirection(DcMotor.Direction.REVERSE);
+
+        twobar1.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
+        twobar1.setPower(0);
+        roller.setPower(0);
+        waitForStart();
+
+        twobar1.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        twobar1.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+        boolean movingTwobar = false;
+        int targetEncoderValue = 0;
+
+        while (opModeIsActive()) {
+            if (gamepad1.dpad_up) {
+                twobar1.setPower(.8);
+            } else if(gamepad1.dpad_down) {
+                twobar1.setPower(.2);
+            } else {
+                if (movingTwobar) {
+                    movingTwobar = false;
+                    targetEncoderValue = twobar1.getCurrentPosition();
+                }
+                if (!twobar1.isBusy()) {
+                    twobar1.setTargetPosition(targetEncoderValue);
+                    twobar1.setPower(.6);
+                }
+            }
+
+        }
+    }
+}
